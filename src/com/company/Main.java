c6329183853549208206c905408b8944604e217d
aidinrakanov <altynaikanatbekova@gmail.com>
2020-02-22 20:42:38 +0600

    update1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,9 @@ public class Main {
         Class1<Integer,Integer> n1 = new Class1<>(4,5);
 
         System.out.println(n1.getNum1() + " " + n1.getNum2());
+        n1.draw();
         n1.nums(4,6);
+
 
     }
 }
